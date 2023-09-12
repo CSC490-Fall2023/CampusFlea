064ff6b0c72b67080c84c2a474f6ddb33663db2f
EvanCorsar <emcorsar@uncg.edu>
2023-09-12 15:10:17 -0400

    init of dependencies

---
+++ CampusFlea/src/main/java/CampusFlea/demo/CampusFleaApplication.java
@@ -0,0 +1,13 @@
+package CampusFlea.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusFleaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusFleaApplication.class, args);
+	}
+
+}
---
+++ CampusFlea/src/test/java/CampusFlea/demo/CampusFleaApplicationTests.java
@@ -0,0 +1,13 @@
+package CampusFlea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusFleaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
