f4aa827038e400cc5d27f5cdd586f4baccbae2d1
EvanCorsar <emcorsar@uncg.edu>
2023-09-19 22:40:45 -0400

    ListController, UserController, model of account, Application Properties modified, index.html/css begin build

---
+++ CampusFlea/src/main/java/CampusFlea/demo/controller/UserController.java
@@ -0,0 +1,11 @@
+package CampusFlea.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+    //@Autowired
+    //private HomeService homeService;
+
+}
