7674aaaca31a5cc890d34567c1b7e6f030dbe8d5
EvanCorsar <emcorsar@uncg.edu>
2023-09-27 19:43:56 -0400

    -Hilla Dependency +FIXED CSS not loading

--- CampusFlea/src/main/java/CampusFlea/demo/controller/HomePageController.java
+++ CampusFlea/src/main/java/CampusFlea/demo/controller/HomePageController.java
@@ -1,15 +1,17 @@
 package CampusFlea.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomePageController {
 
-    @GetMapping("/")
-    public String home(){
-        return "index";
+    private String test;
+
+    @GetMapping("/home")
+    public String home(Model model){
+        model.addAttribute("test", test);
+        return "home";
     }
 }
