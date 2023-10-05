55d197daec765e0d0afa125e4ca6a8bd8db9fe6a
Gene Pressinger <genepressinger@yahoo.com>
2023-10-05 02:22:53 -0400

    Simple login redirect works

--- CampusFlea/src/main/java/CampusFlea/demo/controller/SignInController.java
+++ CampusFlea/src/main/java/CampusFlea/demo/controller/SignInController.java
@@ -1,11 +1,13 @@
 package CampusFlea.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class SignInController {
     @GetMapping("/signin")
-    public String home(Model model) {
+    public String signin(Model model) {
         return "signin";
     }
 }
