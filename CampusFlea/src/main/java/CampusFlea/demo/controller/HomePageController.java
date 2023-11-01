197c8b46ee589b68bc179eea175a49d1a92f3562
EvanCorsar <emcorsar@uncg.edu>
2023-11-01 15:22:16 -0400

    added /buying, /trades to homepage controller

--- CampusFlea/src/main/java/CampusFlea/demo/controller/HomePageController.java
+++ CampusFlea/src/main/java/CampusFlea/demo/controller/HomePageController.java
@@ -204,4 +204,86 @@ public class HomePageController {
 
         return "selling";
     }
+
+    @GetMapping("/buying")
+    public String buying(Model model, HttpSession session) {
+        // Get the user's session key
+        String sessionKey = (String) session.getAttribute("session_key");
+
+        // Check if session key is set
+        if (sessionKey == null) {
+            System.out.println("Did not find session key");
+            return "redirect:/signin";
+        }
+
+        System.out.printf("Found session key: %s\n", sessionKey);
+
+        // Get the user id based on the session key
+        int userId = AccountService.getUserIdFromSessionKey(sessionKey);
+
+        // Check that the session key is valid (redirect them to login otherwise)
+        if (userId == -1) {
+            return "redirect:/signin";
+        }
+
+        // Create the account object from the found userId
+        // load items from saved items in DB
+        Account user = AccountService.getAccount(userId);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+
+        System.out.printf("Logged in (username=%s, email=%s)\n", user.getUsername(), user.getEmail());
+
+        Listing[] listings = ListingService.getAllListings();
+
+        //TODO: ONLY SHOW SELLING CATEGORY
+        for (Listing listing : listings) {
+            System.out.printf("Showing listing (id=%d, title=%s)\n", listing.getId(), listing.getTitle());
+        }
+        //add to model for ThymeLeaf to read
+        model.addAttribute("listings", listings);
+
+        return "buying";
+    }
+
+    @GetMapping("/trades")
+    public String trades(Model model, HttpSession session) {
+        // Get the user's session key
+        String sessionKey = (String) session.getAttribute("session_key");
+
+        // Check if session key is set
+        if (sessionKey == null) {
+            System.out.println("Did not find session key");
+            return "redirect:/signin";
+        }
+
+        System.out.printf("Found session key: %s\n", sessionKey);
+
+        // Get the user id based on the session key
+        int userId = AccountService.getUserIdFromSessionKey(sessionKey);
+
+        // Check that the session key is valid (redirect them to login otherwise)
+        if (userId == -1) {
+            return "redirect:/signin";
+        }
+
+        // Create the account object from the found userId
+        // load items from saved items in DB
+        Account user = AccountService.getAccount(userId);
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+
+        System.out.printf("Logged in (username=%s, email=%s)\n", user.getUsername(), user.getEmail());
+
+        Listing[] listings = ListingService.getAllListings();
+
+        //TODO: ONLY SHOW SELLING CATEGORY
+        for (Listing listing : listings) {
+            System.out.printf("Showing listing (id=%d, title=%s)\n", listing.getId(), listing.getTitle());
+        }
+        //add to model for ThymeLeaf to read
+        model.addAttribute("listings", listings);
+
+        return "trades";
+    }
 }
