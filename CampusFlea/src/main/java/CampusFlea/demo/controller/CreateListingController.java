c8733c565cb31dd96d0424921f59f12338d0ea4e
Gene Pressinger <genepressinger@yahoo.com>
2023-10-31 21:51:12 -0400

    Add button on homepage goes to listing page

---
+++ CampusFlea/src/main/java/CampusFlea/demo/controller/CreateListingController.java
@@ -0,0 +1,12 @@
+package CampusFlea.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CreateListingController {
+    @GetMapping("/createlisting")
+    public String createListing() {
+        return "createlisting";
+    }
+}
