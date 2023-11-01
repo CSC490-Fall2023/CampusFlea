fe25f00d7cd1cd5e4f254dc8309c57181d04c007
Gene Pressinger <genepressinger@yahoo.com>
2023-11-01 15:21:16 -0400

    Users can now edit listings from profile page

--- CampusFlea/src/main/java/CampusFlea/demo/controller/EditListingController.java
+++ CampusFlea/src/main/java/CampusFlea/demo/controller/EditListingController.java
@@ -5,14 +5,14 @@ import CampusFlea.demo.services.ListingService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class EditListingController {
     @GetMapping("/editlisting")
-    public String editListing(Model model) {
+    public String editListing(Model model, @RequestParam int id) {
         // Get the listing
-        int listingId = 1;
-        Listing listing = ListingService.getListing(listingId);
+        Listing listing = ListingService.getListing(id);
 
         // Add the listing to the model
         model.addAttribute("listing", listing);
