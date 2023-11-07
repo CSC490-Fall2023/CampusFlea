c8343c6b865bd12dc7c2427ac63669a36cb48832
Gene Pressinger <genepressinger@yahoo.com>
2023-11-07 10:36:58 -0500

    Implemented posting saving on list view page

--- CampusFlea/src/main/java/CampusFlea/demo/controller/ListingController.java
+++ CampusFlea/src/main/java/CampusFlea/demo/controller/ListingController.java
@@ -4,9 +4,12 @@ import CampusFlea.demo.model.Listing;
 import CampusFlea.demo.services.AccountService;
 import CampusFlea.demo.services.DatabaseService;
 import CampusFlea.demo.services.ListingService;
+import CampusFlea.demo.services.SessionService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.Connection;
@@ -16,12 +19,36 @@ public class ListingController {
 
     @GetMapping("/listing")
     public String list(Model model, @RequestParam String id) {
+        load(model, id);
+        return "listview";
+    }
+
+    @RequestMapping(value = "/listing", params = "save")
+    public String saveListing(Model model, @RequestParam String id, HttpSession session) {
+        // Get current user id
+        int userId = SessionService.getUserIdFromSession(session);
+
+        // Toggle listing save
+        ListingService.toggleSave(userId, id);
+
+        // Load the page
+        load(model, id);
+        return "listview";
+    }
+
+    @RequestMapping(value = "/listing", params = "flag")
+    public String flagListing(Model model, @RequestParam String id) {
+        load(model, id);
+        return "listview";
+    }
+
+    private void load(Model model, String listingId) {
         // Get the listing information
-        int listingId = Integer.parseInt(id);
-        Listing listing = ListingService.getListing(listingId);
+        int listingIdInt = Integer.parseInt(listingId);
+        Listing listing = ListingService.getListing(listingIdInt);
 
         // Add image to listing
-        String image = ListingService.getListingImages(listingId)[0];
+        String image = ListingService.getListingImages(listingIdInt)[0];
         listing.setImage(image);
 
         // Create database connection
@@ -38,6 +65,5 @@ public class ListingController {
         model.addAttribute("category", ListingService.CATEGORIES[listing.getCategory()]);
         model.addAttribute("username", username);
         model.addAttribute("avatar", avatar);
-        return "listview";
     }
 }
